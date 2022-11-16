1d7f758470e41fd864ea549322b8c90967509c61
Fakhirkhan098 <117271296+Fakhirkhan098@users.noreply.github.com>
2022-11-16 16:54:15 +0500

    Add files via upload

---
+++ js assignment 4/app.js
@@ -0,0 +1,436 @@
+    //assignment
+
+    //chapter 1
+//     alert("hello.... this is JS")
+// alert("hello to coding world.." )
+
+// //chapter 2
+
+// var username = "";
+// var Myname = "Fakhir khan";
+// var message = "Hello World";
+// alert(message);
+
+// alert("Pizza \n Pizz \n Piz \n Pi \n P");
+
+// var email = "Fakhirk50@gmail.com";
+// alert("My email address is " + email ); 
+
+
+// var book = "A smarter way to learn JavaScript";
+// alert("I am learning from a book " + book);
+
+// document.write("Yeah! I can write HTML content through javaScript ");
+
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+
+//chapter 3
+
+// var age = "19";
+// alert("I am " + age + " years old");
+
+// var N = "14";
+// alert("You have visited this site " + N + " times")
+
+// var birthyear = "My birth year is 2003";
+// document.write(birthyear);
+
+// var userdata = prompt("Write your name");
+// var order = prompt("How much you want to order");
+// var web = prompt("What do want to order")
+// document.write(userdata + " ordered " + order + web + " on xyz.clothing store ")
+
+
+//chapter 4
+
+// var c = 5;
+// var d = 5;
+// var x = 6;
+// var y = 6;
+// var g = 8;
+// var h = 10;
+
+// if (c === d) {
+//  if (x === y) {
+//  g = h;
+//  }
+//      }
+
+
+// var myname = "Fakhir khan";
+// var myName = "Fakhir khan";
+// var my_name = "Fakhir khan";
+// var _my_name = "Fakhir khan";
+// var $my_name = "Fakhir khan";
+
+// var @myname = "Fakhir khan";
+// var 123myName = "Fakhir khan";
+// var my name = "Fakhir khan";
+// var month&year = "Fakhir khan";
+// var birth-year = "2003";
+
+
+// document.write("<h3>Rules for naming js variables</h3>" + "1)A heading stating Rules for naming JS variables <br>" + "2)Variable names can only contain ______, ______,_____and_____- .For example $my_1stVariable <br>" + "3) Variables must begin with a ______, ______ or_____. For example $name, _name or name <br>" + "4) Variable names are case _________ <br>" + "5) Variable names should not be JS _________ <br>");
+
+
+//chapter 5 
+
+// var num1 = 3;
+// var num2 = 4;
+// var sum = num1 + 5-num2;
+// console.log(sum)
+
+// var num1 = 3;
+// var num2 = 4;
+// var sum = num1 - num2;
+// console.log(sum)
+
+// var num1 = 3;
+// var num2 = 4;
+// var sum = num1 * num2;
+// console.log(sum)
+
+// var num1 = 3;
+// var num2 = 4;
+// var sum = num1 / num2;
+// console.log(sum)
+
+// var ticket = 600;
+// var quantity = prompt("how much tickets");
+// var Total = ticket * quantity ;
+// document.write("Total cost of buying " + quantity + " ticket is " +  Total) 
+
+// var userinput = +prompt("enter a table you want to see")
+// var userlength = +prompt("Enter The length of the table")
+
+// // for(var i = 1; i<userlength+1; i++){
+// // document.write(userinput + "x" + i + "=" + userinput*i + "</br>")
+// // }
+
+// for(var i = 1; i<userlength+1; i++){
+//     document.write(userinput + "x" + i + "=" + userinput*i + "</br>")
+// }
+
+
+// var item1  = 650;
+// var item2 = 750;
+// var quantity1 = prompt("quantity");
+// var Quantity2 = prompt("quantity2");
+// var Shippingcharges = 200;
+
+// document.write("Item 1 cost " + item1 + " Item 2 cost " + item2  + quantity1 + Quantity2 + Shippingcharges );
+
+
+
+//marksheet
+// var oops;
+// var com;
+// var ict;
+// var eng;
+// var maths;
+// var total;
+// var per;
+
+// oops = + prompt("enter your oops marks");
+// com = + prompt("enter your com marks");
+// ict = + prompt("enter your ict marks");
+// eng = + prompt("enter your eng marks");
+// maths = + prompt("enter your maths marks");
+
+// total = (oops + com + ict + eng + maths);
+// per = (total / 500) * 100;
+
+// console.log(oops);
+// console.log(com);
+// console.log(ict);
+// console.log(eng);
+// console.log(maths);
+// console.log(total);
+// console.log(per);
+
+
+// if (per >= 80) {
+//     console.log("Grade = A+");
+// }
+// else if (per < 80 && per >= 70) {
+//     console.log("Grade = A");
+// }
+// else if (per < 70 && per >= 60) {
+//     console.log("Grade = B");
+// }
+// else if (per < 60 && per >= 50) {
+//     console.log("Grade = C");
+// }
+// if (per < 50 && per >= 10) {
+//     console.log("Grade = F");
+// }
+
+//9
+// var a = prompt("enter value");
+// var b = prompt("enter value");
+// var c = prompt("enter value");
+// var d = prompt("enter value");
+// var e = prompt("enter value");
+// document.write(a + b * c - d / e);
+
+//10
+    // var dob = new Date(prompt("Enter your date of birth","jan 1 1970"));
+    // var dobmili = dob.getTime();
+    // var today = new Date();
+    // var todaymili = today.getTime();
+    // var diff = todaymili - dobmili;
+    // var accuage = math.floor(diff/(1000*60*60*24*30*12));
+    // console.log(accuage);
+    
+//chapter6/9
+
+
+// var oops;
+// var com;
+// var ict;
+// var eng;
+// var maths;
+// var total;
+// var per;
+
+// oops = + prompt("enter your oops marks");
+// com = + prompt("enter your com marks");
+// ict = + prompt("enter your ict marks");
+// eng = + prompt("enter your eng marks");
+// maths = + prompt("enter your maths marks");
+
+// total = (oops + com + ict + eng + maths);
+// per = (total / 500) * 100;
+
+// document.write(oops);
+// document.write(com);
+// document.write(ict);
+// document.write(eng);
+// document.write(maths);
+// document.write(total);
+// document.write(per);
+
+
+//chapter 9/11
+
+// var cityname = prompt("enter city name")
+// if(cityname === "karachi"){
+//     alert("Welcome To The City Of DAKAITS")
+
+// }
+// else{
+//     alert("you are good to goo..")
+// }
+
+
+
+// var pro = prompt("Enter Gender");
+// if(pro === "male"){
+//     alert("Welcome Sirr")
+  
+// }
+// else{
+//     alert("Welcome Madam")
+// }
+
+// var signel = prompt("Red,yellow,green");
+
+// if(signel === "red"){
+//     alert("Must Stop")
+// }
+// else if(signel === "yellow"){
+//     alert("Ready to go")
+// }
+// else{
+//     alert("Move on")
+// }
+
+// var f = prompt("remaining fuel in car");
+// if(f > 0.25){
+//     alert("Good to GO")
+// }
+// else if(f < 0.25 || 0.25 ){
+//     alert("“Please refill the fuel in your car”")
+// }
+
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+//  var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+//  var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+// if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+
+
+//marksheet
+// var oops;
+// var com;
+// var ict;
+// var eng;
+// var maths;
+// var total;
+// var per;
+
+// oops = + prompt("enter your oops marks");
+// com = + prompt("enter your com marks");
+// ict = + prompt("enter your ict marks");
+// eng = + prompt("enter your eng marks");
+// maths = + prompt("enter your maths marks");
+
+// total = (oops + com + ict + eng + maths);
+// per = (total / 500) * 100;
+
+// console.log(oops);
+// console.log(com);
+// console.log(ict);
+// console.log(eng);
+// console.log(maths);
+// console.log(total);
+// console.log(per);
+
+
+// if (per >= 80) {
+//     console.log("Grade = A+");
+// }
+// else if (per < 80 && per >= 70) {
+//     console.log("Grade = A");
+// }
+// else if (per < 70 && per >= 60) {
+//     console.log("Grade = B");
+// }
+// else if (per < 60 && per >= 50) {
+//     console.log("Grade = C");
+// }
+// if (per < 50 && per >= 10) {
+//     console.log("Grade = F");
+// }
+
+
+// var a = prompt("Enter secret number 1 to 10")
+// if(a == 5 ){
+//     alert("bingo! Correct")
+// }
+// else{
+//     alert("AWWW Wrong!")
+// }
+
+
+// var a = prompt("enter a number");
+// var b = 3;
+// if(a / b ){
+//     alert("Yes this num is divisible by 3")
+// }
+// else{
+//     alert("No the number is not divisible by 3")
+// }
+
+// var T = prompt("Enter the temprature!")
+
+// if(T > 40){
+//     alert(" “It is too hot outside.”")
+// }
+// else if(T > 30){
+//     alert(" “The Weather today is Normal.” ")
+// }
+// else if(T > 20){
+//     alert(" Today's Weather is cool.")
+// }
+// else if(T > 10){
+//     alert(" “OMG! Today's weather is so Cool.”")
+// }
+
+	
+// var num1 = prompt("enter the number"); 
+// var num2 = prompt("enter the number");
+
+
+
+
+// var city = prompt("Enter Your City Name")
+// // city = city.toLowerCase()
+// var firstchar = city.slice(0,1);
+// firstchar = firstchar.toUpperCase()
+// var otherChar = city.slice(1)
+// otherChar = otherChar.toLowerCase()
+
+// var city = firstchar + otherChar;
+// console.log(city)
+
+
+// var num1 = prompt("enter the number");
+// if( num1<10 ){
+//     alert("Negative")
+// } 
+// else{
+//     alert("positive" || 0)
+// }
+
+
+// var greeting =  prompt("Enter hour" )
+// var hour = 13;
+// if (hour < 18) {
+// alert(greeting = "Good day")}
+// else{
+// alert(greeting = "Good evening")
+// }
+
+
+// var time = 1900;
+// flag = true;
+// if (time >= 0000 && time < 1200) {
+// if(true)
+// alert("Good Morning")
+// }
+// if(false)
+// if(time >= 1200 && time < 1700){
+// if(true)
+// alert("Good Afternoon")
+// }
+// if(false)
+// if(time >= 1700 && time < 2100){
+//     if(true)
+//     alert("Good Evening")
+// }
+// if(false)
+// if(time >= 2100 && time < 2359){
+//     if(true)
+//     alert("Good Night")
+// }
+
+
+// var arr = ["a","b","c"];
+// var arr = [1,2,3];
+// var arr = ["","","",""];
+// var arr = ["a",1,"b",2,"c",3];
+// var arr = [ "SSC", "HSC", "BCS","BS", "BCOM", "MS", "M. Phil.", "PhD"];
+// document.write(arr)
